831d80f71bd517a0dd42d705eba12e18f0ae5705
francesco chini <francesco.chini1990@gmail.com>
2021-01-18 12:05:09 +0100

    generating a square
    On this branch I want to solve the problem of generating a square

--- experiment_area.py
+++ experiment_area.py
@@ -2,6 +2,8 @@
 # before changing the other files
 
 import numpy as np 
+import random as rnd
+
 import functions as fn
 
 board = np.array([[3, 4, 1, 0, 2],
@@ -14,24 +16,9 @@ print(board)
 
 perm = fn.rand_perm(5)
 a = np.array(perm)
+a = a.astype(int)
 print(perm)
 
-n = 3
-print(n)
-print("the index of ")
-print(n)
-print("is ")
-index = np.where(a==n)[0][0]
-print(index)
-
-c = np.array([])
-
-for i in range(5):
-	c = np.append(c, a[(i+index)%5])
-
-c = c.astype(int)
-
-print(c)
 
 # Trasform the mess above in a function
 
@@ -43,4 +30,60 @@ def cyclic_perm(arr, numb):
 	new_arr = new_arr.astype(int)
 	return new_arr
 
-print(cyclic_perm(a, 3))
+#--------------------------------------
+
+new_board = np.zeros([5, 5]).astype(int)
+
+for col in range(5):
+	arr = board[:, col]
+	asd = cyclic_perm(arr, a[col])
+	for row in range(5):
+			new_board[row, col] = asd[row]
+
+print(new_board)
+
+
+# Minchia.... non sembra funzionare
+
+
+#---------------------------------
+# Provo un altro approccio. Invece che 
+# partire gia da una matrice, la creo da zero
+
+
+n = 5
+
+new_board = np.zeros([n, n]).astype(int)
+# first row totally random
+new_board[0,:] = fn.rand_perm(n)
+print(new_board)
+# secon row 
+# create a list with the elements
+list_num = range(n)
+for c in range(n):
+	elem_up = new_board[0,c]
+	was_elem_up_removed = 0
+	if elem_up in list_num:
+		list_num.remove(elem_up) # I remove from the pool the element in the same column 
+		was_elem_up_removed = 1
+	new_el = rnd.choice(list_num)
+	print(new_el)
+	new_board[1,c] = new_el
+	if was_elem_up_removed == 1:
+		list_num.append(elem_up) # We put the number above in the column back in the pool
+	if new_el in list_num:
+		list_num.remove(new_el) # We don't want repetitions on the row
+	
+
+# FUNZIONA ECCETTO NEL CASO IN CUI L'ULTIMO ELEMENTO DELLA 
+# SECONDA RIGA E' FORZATO AD ESSERE UGUALE A QUELLO DELLA PRIMA
+
+# To do: risolvere il bug! Idea: fermarsi uno step prima e controllare gli ultimi due numeri 
+# Trovare un modo (ricorsivo?) per fare le altre righe
+
+print(new_board)
+
+# ALTRA IDEA: riempire la matrice un elemento alla volta provando casualmente ogni numero in 
+# range(n) finche uno non va bene. Scrivere una funzione di check.. magari tenere traccia 
+# degli elementi già modificati con una matrice di 0 e 1
+
