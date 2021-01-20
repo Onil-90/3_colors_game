f34944adc9720bd70917b2a4ffec59ae540cd22b
francesco chini <francesco.chini1990@gmail.com>
2021-01-20 22:50:26 +0100

    Edited experiment area and added gui file
    I have finally manged to create a function which generates a board.
    This function is still in the experiment file. It is improvable
    and also the function check() in the function file needs to be made scalable
    with n.
    I have created the file gui to play around with tkinker.

--- experiment_area.py
+++ experiment_area.py
@@ -6,84 +6,45 @@ import random as rnd
 
 import functions as fn
 
-board = np.array([[3, 4, 1, 0, 2],
-		 		 [4, 0, 2, 3, 1],
-		 		 [2, 3, 0, 1, 4],
-		 		 [1, 2, 3, 4, 0],
-		 		 [0, 1, 4, 2, 3]])
+
+#  New
+def create_pool(n, row_elem, column_elem, check_board, board):
+	col = board[row_elem, check_board[row_elem, :]]
+	row = board[check_board[:, column_elem], column_elem]
+	pool = [x for x in range(n) if (x not in col and x not in row)]
+	return pool
+
+
+# Fill the upper left triangle
+def fill(n):
+	new_board = np.zeros([n, n]).astype(int)
+	# Initialize the matrix check_board to keep
+	# track of the entries that have been added
+	check_board = np.zeros([n, n]).astype(bool)
+	for r in range(n):
+		for c in range(n):
+			pool = create_pool(n, r, c, check_board, new_board)
+			if pool == []:    # If there is nothing to choose it means that we made a mistake on an earlier step
+				break
+			elem = rnd.choice(pool)
+			new_board[r,c] = elem
+			check_board[r, c] = True
+
+	return new_board
+
+n = 5
+board = np.zeros([n,n]).astype(int)
+while fn.check(board) == False:
+	board = fill(5)
+
 
 print(board)
 
-perm = fn.rand_perm(5)
-a = np.array(perm)
-a = a.astype(int)
-print(perm)
+# Now we have to solve the rest of the square
 
+def score(n, row_elem, column_elem, check_board):
+	col = sum(check_board[row_elem,:])
+	row = sum(check_board[:,column_elem])
+	return max(col, row)
 
-# Trasform the mess above in a function
-
-def cyclic_perm(arr, numb):
-	new_arr = np.array([])
-	index = np.where(arr ==numb)[0][0]
-	for i in range(5):
-		new_arr = np.append(new_arr, arr[(i +index)%5])
-	new_arr = new_arr.astype(int)
-	return new_arr
-
-#--------------------------------------
-
-new_board = np.zeros([5, 5]).astype(int)
-
-for col in range(5):
-	arr = board[:, col]
-	asd = cyclic_perm(arr, a[col])
-	for row in range(5):
-			new_board[row, col] = asd[row]
-
-print(new_board)
-
-
-# Minchia.... non sembra funzionare
-
-
-#---------------------------------
-# Provo un altro approccio. Invece che 
-# partire gia da una matrice, la creo da zero
-
-
-n = 5
-
-new_board = np.zeros([n, n]).astype(int)
-# first row totally random
-new_board[0,:] = fn.rand_perm(n)
-print(new_board)
-# secon row 
-# create a list with the elements
-list_num = range(n)
-for c in range(n):
-	elem_up = new_board[0,c]
-	was_elem_up_removed = 0
-	if elem_up in list_num:
-		list_num.remove(elem_up) # I remove from the pool the element in the same column 
-		was_elem_up_removed = 1
-	new_el = rnd.choice(list_num)
-	print(new_el)
-	new_board[1,c] = new_el
-	if was_elem_up_removed == 1:
-		list_num.append(elem_up) # We put the number above in the column back in the pool
-	if new_el in list_num:
-		list_num.remove(new_el) # We don't want repetitions on the row
-	
-
-# FUNZIONA ECCETTO NEL CASO IN CUI L'ULTIMO ELEMENTO DELLA 
-# SECONDA RIGA E' FORZATO AD ESSERE UGUALE A QUELLO DELLA PRIMA
-
-# To do: risolvere il bug! Idea: fermarsi uno step prima e controllare gli ultimi due numeri 
-# Trovare un modo (ricorsivo?) per fare le altre righe
-
-print(new_board)
-
-# ALTRA IDEA: riempire la matrice un elemento alla volta provando casualmente ogni numero in 
-# range(n) finche uno non va bene. Scrivere una funzione di check.. magari tenere traccia 
-# degli elementi già modificati con una matrice di 0 e 1
 
